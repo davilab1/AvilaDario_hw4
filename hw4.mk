4834c4c4e0b82fd656fb80f28f19c6bb0799b95e
Dario Avila <DarioAvila@Darios-MacBook-Air.local>
2018-11-12 10:19:48 -0500

    .

--- hw4.mk
+++ hw4.mk
@@ -1,7 +1,10 @@
 
 
 
-Resultados_hw4.pdf:Plots_hww4.py ode.out pde.out
+Resultados_hw4.pdf:Plots_hww4.py ode.out pde.out Resultados_hw4.tex
+	pdflatex Resultados_hw4.tex
+
+Plots_hww4.py:datos1.dat datos2.dat datos3.dat
 	python3 Plots_hww4.py
 
 datos1.dat: ode.out
